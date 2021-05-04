0caa3452cf14d3521c8134c2e6d2e77297eedf6d
c3p0 <dhaiouir.khalid@gmail.com>
2021-05-04 15:42:55 +0200

    create port_scanner

---
+++ port_scanner.go
@@ -0,0 +1,36 @@
+package main 
+import(
+	"log"
+	"net"
+	"strconv"
+	"time"
+)
+
+var ipToScan = os.Args[1]
+var minPort = 1
+var maxPort = 1024 
+
+func main() { 
+	activeThreads := 0 
+	donChannel := make(chan bool)
+
+	for port := minPort; port <= maxPort; port++{
+		go testTCPConnection(ipToScan, port, donChannel)
+		activeThreads ++
+	}
+	// wait for all threads to finish 
+	for activeThreads > 0 {
+		<-donChannel
+		activeThreads--
+	}
+}
+func testTCPConnection(ip string, port int, donChannel chan bool){
+	_, err := net.DialTimeout("tcp", ip+":"+strconv.Itoa(port),
+	time.Second*10)
+
+	if err == nil {
+		log.Printf("Host %s has open port : %d\n", ip,port)
+
+	}
+	donChannel <- true 
+}
